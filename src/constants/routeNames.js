91b921d9313b023f4d233a170a338bee2cdb9adc
Edris <edris.jalaly@afghantelecom.af>
2025-08-20 17:35:52 +0430

    Add basic routes for routing

---
+++ src/constants/routeNames.js
@@ -0,0 +1,12 @@
+export const APP_ROUTE_NAMES = Object.freeze({
+  HOME: 'home', // Home
+  PRODUCT_LIST: 'product-list', // Product List
+  PRODUCT_CREATE: 'product-create', // Create Product
+  PRODUCT_UPDATE: 'product-update', // Update Product
+  SIGN_UP: 'sign-up', // Sign Up
+  SIGN_IN: 'sign-in', // Sign In
+  ACCESS_DENIED: 'access-denied', // Access Denied
+  CONTACT_US: 'contact-us', // Contact Us
+  NOT_FOUND: 'not-found',
+  ABOUT_US: 'About-us',
+})
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { APP_ROUTE_NAMES } from '../constants/routeNames'
+import Home from '@/views/Home/Home.vue'
+import SignIn from '@/views/auth/SignIn.vue'
+import NotFound from '@/views/auth/NotFound.vue'
+import AcceessDenied from '@/views/auth/AcceessDenied.vue'
+import ContactUs from '@/views/Home/ContactUs.vue'
+import ProductList from '@/views/Product/ProductList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: APP_ROUTE_NAMES.HOME,
+      component: Home,
+    },
+    {
+      path: '/sign-in',
+      name: APP_ROUTE_NAMES.SIGN_IN,
+      component: SignIn,
+    },
+    {
+      path: '/sing-up',
+      name: APP_ROUTE_NAMES.ACCESS_DENIED,
+      component: AcceessDenied,
+    },
+    {
+      path: '/not-found',
+      name: APP_ROUTE_NAMES.NOT_FOUND,
+      component: NotFound,
+    },
+    {
+      path: '/contact-us',
+      name: APP_ROUTE_NAMES.CONTACT_US,
+      component: ContactUs,
+    },
+    {
+      path: '/product-list',
+      name: APP_ROUTE_NAMES.PRODUCT_LIST,
+      component: ProductList,
+    },
+  ],
 })
 
 export default router
